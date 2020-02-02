cbbffb144c95d6d858732a9d10602f4a606244ae
Anastasiia <nastyona221985@gmail.com>
2020-02-02 14:20:38 +0200

    homework-3

---
+++ task-01.js
@@ -0,0 +1,20 @@
+"use strict";
+const user = {
+  name: "Mango",
+  age: 20,
+  hobby: "html",
+  premium: true,
+  changeHobby(newHobby) {
+    this.hobby = newHobby;
+  }
+};
+
+user.changeHobby("skydiving");
+user.mood = "happy";
+user.premium = false;
+
+const keys = Object.keys(user);
+
+for (const key of keys) {
+  console.log(`${key} : ${user[key]}`);
+}
